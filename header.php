6b913e266f0788cb15af5298116e8be77892d762
Rishija <rishijamangla@gmail.com>
2018-04-21 21:54:14 +0530

    Create index.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <html>
     <head>
-        <title> Fitness Freak</title>
+        <title>Fitness Freak</title>
         <meta charset = "utf-8">
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
         <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
-        <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> </script>
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     </head>
     <body>
@@ -13,67 +13,45 @@
         <nav class = "navbar navbar-inverse">
             <div class = "container-fluid">
                 <div class = "navbar-header">
-                    <a class = "navbar-brand" href = "index.php"> Fitness Freak</a>
+                    <a class = "navbar-brand" href = "index.php">Fitness Freak</a>
                 </div>
                 <ul class = "nav navbar-nav">
-                    <li class = "active"> <a href = "#"> About</a> </li>
-                    <li> <a href = "#"> Facilities</a> </li>
-                    <li> <a href = "#"> Our trainers</a> </li>
-                    <li> <a href = "#"> Achievements</a> </li>
-                    <li> <a href = "#"> Gallery</a> </li>
-                    <li> <a href = "#"> Contact</a> </li>
+                    <li class = "active"><a href = "#">About</a></li>
+                    <li><a href = "#">Facilities</a></li>
+                    <li><a href = "#">Our trainers</a></li>
+                    <li><a href = "#">Achievements</a></li>
+                    <li><a href = "#">Gallery</a></li>
+                    <li><a href = "#">Contact</a></li>
                 </ul>
                 <ul class = "nav navbar-nav navbar-right">
                     <?php
                       session_start();
                       if(!isset($_SESSION['uid'])) {
-                        echo '
-                        <li> <a href = "signup.php"> <span class = "glyphicon glyphicon-user"> </span> Sign Up</a> </li>
-                        <li> <a href = "login.php"> <span class = "glyphicon glyphicon-log-in"> </span> Login</a> </li> ';
+                    ?>
+                        <li class = "dropdown"><a class = "dropdown-toggle" data-toggle = "dropdown"><span class = "glyphicon glyphicon-user"></span> Sign Up<span class = "caret"></span></a>
+                          <ul class = "dropdown-menu" style = "text-align: center;">
+                            <li><a href = "#">Join us</a></li>
+                            <li><a href = "#">Join as trainer</a></li>
+                          </ul>
+                        </li>
+                        <li class = "dropdown"><a class = "dropdown-toggle" data-toggle = "dropdown"><span class = "glyphicon glyphicon-log-in"></span> Login<span class="caret"></span></a>
+                          <ul class = "dropdown-menu" style = "text-align: center;">
+                            <li><a href = "login/login.php">Customer</a></li>
+                            <li><a href = "#">Trainer</a></li>
+                            <li><a href = "#">Admin</a></li>
+                          </ul>
+                        </li>
+                    <?php
                       }
                       else {
-                        echo '
-                        <li> <a href = "#"> <span class = "glyphicon glyphicon-user"> Account</span> </a> </li>
-                        <li> <a href = "login.php"> <span class = "glyphicon glyphicon-log-out"> </span> Logout</a> </li> ';
+                    ?>
+                        <li><a href = "#"><span class = "glyphicon glyphicon-user">Account</span></a></li>
+                        <li><a href = "login.php"><span class = "glyphicon glyphicon-log-out"></span>Logout</a></li>
+                    <?php
                       }
                     ?>
                 </ul>
             </div>
         </nav>
-
-
-        <div class = "container" style = "width: 100%; height: 50%;">
-            <div id = "myCarousel" class = "carousel slide" data-ride = "carousel">
-                <!-- Indicators -->
-                <ol class = "carousel-indicators">
-                    <li data-target = "#myCarousel" data-slide-to = "0" class = "active"> </li>
-                    <li data-target = "#myCarousel" data-slide-to = "1"> </li>
-                    <li data-target = "#myCarousel" data-slide-to = "2"> </li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class = "carousel-inner" style = "width: 100%; height: 100%;">
-                    <div class = "item active" >
-                        <img src = "./images/1.jpg" alt = "Los Angeles">
-                    </div>
-
-                    <div class = "item">
-                        <img src = "./images/2.jpg" alt = "Chicago">
-                    </div>
-
-                    <div class = "item">
-                        <img src = "./images/3.jpg" alt = "New york">
-                    </div>
-                </div>
-
-                <!-- Left and right controls -->
-                <a class = "left carousel-control" href = "#myCarousel" data-slide = "prev">
-                    <span class = "glyphicon glyphicon-chevron-left"> </span>
-                </a>
-                <a class = "right carousel-control" href = "#myCarousel" data-slide = "next">
-                    <span class = "glyphicon glyphicon-chevron-right"> </span>
-                </a>
-            </div>
-        </div>
     </body>
 </html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('header.php');
+?>
+
+<div class = "container" style = "width: 100%; height: 50%;">
+    <div id = "myCarousel" class = "carousel slide" data-ride = "carousel">
+        <!-- Indicators -->
+        <ol class = "carousel-indicators">
+            <li data-target = "#myCarousel" data-slide-to = "0" class = "active"></li>
+            <li data-target = "#myCarousel" data-slide-to = "1"></li>
+            <li data-target = "#myCarousel" data-slide-to = "2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class = "carousel-inner" style = "width: 100%; height: 100%;">
+            <div class = "item active" >
+                <img src = "./images/1.jpg" alt = "Los Angeles">
+            </div>
+
+            <div class = "item">
+                <img src = "./images/2.jpg" alt = "Chicago">
+            </div>
+
+            <div class = "item">
+                <img src = "./images/3.jpg" alt = "New york">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class = "left carousel-control" href = "#myCarousel" data-slide = "prev">
+            <span class = "glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class = "right carousel-control" href = "#myCarousel" data-slide = "next">
+            <span class = "glyphicon glyphicon-chevron-right"></span>
+        </a>
+    </div>
+</div>
