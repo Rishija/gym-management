48886415fb35e26b0b9f57b9e640fc9cbaa6b393
Rishija <rishijamangla@gmail.com>
2018-04-21 20:53:12 +0530

    Add dietchart and limit

--- dbinit.php
+++ dbinit.php
@@ -1,16 +1,22 @@
 <?php
+
 $serverName = "localhost";
 $userName = "root";
 $password = "root";
 $conn = mysqli_connect($serverName, $userName, $password);
+
 if($conn) {
+
   $dbname = "gym";
   $createDb = "CREATE DATABASE $dbname";
   $db = mysqli_query($conn, $createDb);
+
   if($db) {
+
     echo "Database created! <br>";
     if(mysqli_select_db($conn, $dbname)) {
       echo "Database \"$dbname\" is in use <br>";
+
       $table = "CREATE TABLE members (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(30) NOT NULL UNIQUE,
@@ -20,12 +26,15 @@ if($conn) {
         phone VARCHAR(10) NOT NULL,
         joined DATETIME
       );";
+
       $tableCreated = mysqli_query($conn, $table);
       if($tableCreated) {
         echo "Table 'members' created successfully<br>";
-      } else {
+      }
+      else {
         echo "Can't create Table 'members'";
       }
+
       $table = "CREATE TABLE trainer (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(30) UNIQUE NOT NULL,
@@ -35,16 +44,20 @@ if($conn) {
         experience INT(2) NOT NULL,
         salary INT(5),
         type VARCHAR(10),
+        clientAssigned int(2),
         FOREIGN KEY (username) REFERENCES members(username)
       );";
+
       $tableCreated = mysqli_query($conn, $table);
       if($tableCreated) {
         echo "Table 'trainer' created successfully<br>";
-      } else {
+      }
+      else {
         echo "Can't create Table 'trainer'";
-            }
+      }
+
       $table = "CREATE TABLE client (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
         username VARCHAR(30) UNIQUE NOT NULL,
         password VARCHAR(256) NOT NULL,
         weight INT(3),
@@ -53,13 +66,16 @@ if($conn) {
         purpose VARCHAR(20),
         medicalHistory VARCHAR(100),
         trainer VARCHAR(30) UNIQUE,
+        chart BLOB,
         FOREIGN KEY (username) REFERENCES members(username),
         FOREIGN KEY (trainer) REFERENCES trainer(username)
       );";
+
       $tableCreated = mysqli_query($conn, $table);
       if($tableCreated) {
         echo "Table 'client' created successfully<br>";
-      } else {
+      }
+      else {
         echo "Can't create Table 'client'";
       }
       // Create table admin and add root entry
@@ -71,7 +87,8 @@ if($conn) {
   else {
     echo "Couldn't create database";
   }
-} else {
+}
+else {
   echo "Failed to connect!";
 }
 $conn.close();
