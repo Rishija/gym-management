c17fc1039b136d90c3f249700fc4fc475de83e3a
ShikhaChauhan <shikhachauhan.nd@gmail.com>
2018-04-22 01:17:29 +0530

    cleanup

--- login/login_admin.php
+++ login/login_admin.php
@@ -1,38 +1,11 @@
 <?php
 
+    require_once('../header.php'); 
     require_once('./loginHeader.php');
-    require_once('../header.php');
-    // require('includes/loginI.php');
-
+    // require('includes/admin_loginI.php');
     $status = $_GET['login'];
-
-    // Wrong password
-    if($status == "wp") {
-      echo '
-      <div class="alert alert-danger">
-        <strong>Error!</strong> Email id or password doesn\'t match
-      </div>
-      ';
-    }
-    // Not registered
-    elseif($status == "nr"){
-      echo '
-      <div class="alert alert-danger">
-        <strong>Error!</strong> Email id not registered
-      </div>
-      ';
-    }
-    // Invalid Email
-    elseif($status == "ie"){
-      echo '
-      <div class="alert alert-danger">
-        <strong>Error!</strong> Enter a valid email
-      </div>
-      ';
-    }
-
-    echo "Hurray!";
 ?>
+
 
 <form action = "./includes/admin_loginI.php">
   <div class = "imgcontainer">
