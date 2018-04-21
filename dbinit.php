e861978f5cb63081a37cc4a42a9b115d83e3b3ad
Rishija <rishijamangla@gmail.com>
2018-04-21 16:50:50 +0530

    Fix create db

--- dbinit.php
+++ dbinit.php
@@ -1,25 +1,16 @@
 <?php
-
 $serverName = "localhost";
 $userName = "root";
 $password = "root";
-
 $conn = mysqli_connect($serverName, $userName, $password);
-
 if($conn) {
-
   $dbname = "gym";
   $createDb = "CREATE DATABASE $dbname";
   $db = mysqli_query($conn, $createDb);
-
   if($db) {
-
     echo "Database created! <br>";
-
     if(mysqli_select_db($conn, $dbname)) {
-
       echo "Database \"$dbname\" is in use <br>";
-
       $table = "CREATE TABLE members (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(30) NOT NULL UNIQUE,
@@ -29,18 +20,16 @@ if($conn) {
         phone VARCHAR(10) NOT NULL,
         joined DATETIME
       );";
-
       $tableCreated = mysqli_query($conn, $table);
-      if(tableCreated) {
+      if($tableCreated) {
         echo "Table 'members' created successfully<br>";
       } else {
-        echo "Can't create 1";
+        echo "Can't create Table 'members'";
       }
-
       $table = "CREATE TABLE trainer (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(30) UNIQUE NOT NULL,
-        password VARCHAR(20) NOT NULL,
+        password VARCHAR(256) NOT NULL,
         specialization VARCHAR(20) NOT NULL,
         qualification VARCHAR(20) NOT NULL,
         experience INT(2) NOT NULL,
@@ -48,18 +37,16 @@ if($conn) {
         type VARCHAR(10),
         FOREIGN KEY (username) REFERENCES members(username)
       );";
-
       $tableCreated = mysqli_query($conn, $table);
-            if(tableCreated) {
+      if($tableCreated) {
         echo "Table 'trainer' created successfully<br>";
       } else {
         echo "Can't create Table 'trainer'";
             }
-
       $table = "CREATE TABLE client (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(30) UNIQUE NOT NULL,
-        password VARCHAR(20) NOT NULL,
+        password VARCHAR(256) NOT NULL,
         weight INT(3),
         height FLOAT NOT NULL,
         fee INT(5),
@@ -69,14 +56,12 @@ if($conn) {
         FOREIGN KEY (username) REFERENCES members(username),
         FOREIGN KEY (trainer) REFERENCES trainer(username)
       );";
-
       $tableCreated = mysqli_query($conn, $table);
-      if(tableCreated3) {
+      if($tableCreated) {
         echo "Table 'client' created successfully<br>";
       } else {
         echo "Can't create Table 'client'";
       }
-
       // Create table admin and add root entry
     }
     else {
@@ -89,7 +74,5 @@ if($conn) {
 } else {
   echo "Failed to connect!";
 }
-
 $conn.close();
-
 ?>
