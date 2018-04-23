d2d357ffe26dd9a1ee798d090f988a0be34b556f
ShikhaChauhan <shikhachauhan.nd@gmail.com>
2018-04-23 17:08:19 +0530

    Handle admin login

--- login/login_admin.php
+++ login/login_admin.php
@@ -1,23 +1,28 @@
 <?php
-
-    require_once('../includes/header.php');
-    require_once('./loginHeader.php');
-    // require('includes/admin_loginI.php');
-    $status = $_GET['login'];
+  require('./loginHeader.php');
+  require('../includes/header.php');
+  $status = $_GET['login'];
+  if($status == "wd") {
+    ?>
+    <div class="alert alert-danger">
+      <strong>Error!</strong> Invalid admin details
+    </div>
+  <?php
+  }
 ?>
 
 
-<form action = "./includes/admin_loginI.php">
-  <div class = "imgcontainer">
-    <img src = "../images/logo.png" class = "dp">
+<form action = "./includes/admin_loginI.php" method = "POST">
+  <div align = "center">
+    <img src = "../images/logo.png">
   </div>
 
   <div class = "container">
     <label for = "uname"><b>Username</b></label>
     <input type = "text" placeholder = "Enter Username" name = "name" required>
 
-    <label for = "psw"><b>Password</b></label>
-    <input type = "password" placeholder = "Enter Password" name = "password" required>
+    <label for = "psw"><b>password</b></label>
+    <input type = "password" placeholder = "Enter key" name = "password" required>
 
     <label for = "adminKey"><b>AdminKey</b></label>
     <input type = "password" placeholder = "Enter key" name = "adminKey" required>
