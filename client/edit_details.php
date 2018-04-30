1364e5b5c1b078bb5b7f6cc9dcfc45507a05bd18
Sudhanshu Vishnoi <sidvishnoi8@gmail.com>
2018-04-30 21:04:26 +0530

    :sparkles: Add support for autofill `<select>`
    :shirt: and a lot of cleanup

--- client/edit_details.php
+++ client/edit_details.php
@@ -1,5 +1,4 @@
 <?php
-
   session_start();
   if($_SESSION['type'] != "client") {
     header("Location: ../index.php");
@@ -10,26 +9,19 @@
   require('../includes/db.php');
 
   $status = $_GET['edit'];
-
-  if($status == "success") {
 ?>
 
-    <div class="alert alert-success">
-      <strong>Success!</strong> Information updated successfully
-    </div>
-<?php
-
-    }
-  elseif($status == "error") {
-?>
-
-<div class="alert alert-danger">
-  <strong>Error!</strong> Could not update details
-</div>
+<?php if($status == "success") { ?>
+  <div class="alert alert-success">
+    <strong>Success!</strong> Information updated successfully
+  </div>
+<?php } elseif($status == "error") { ?>
+  <div class="alert alert-danger">
+    <strong>Error!</strong> Could not update details
+  </div>
+<?php } ?>
 
 <?php
-  }
-
   $getDetails = "SELECT fname, lname, m.username, email, phone, joined,
   password, weight, height, fee, purpose, history, trainer, chart
   from client c, members m
@@ -47,145 +39,125 @@
   mysqli_stmt_close($stmt1);
 ?>
 
-<link rel = "stylesheet" href = "/gym_management/assets/css/font-awesome.min.css"></link>
+<link rel="stylesheet" href="/gym_management/assets/css/font-awesome.min.css"></link>
 <link rel="stylesheet" type="text/css" href="/gym_management/assets/css/client.css"></link>
 
 
 <form action = "./includes/edit_detailsI.php" method = "POST" style = "max-width:500px;margin:auto">
   <h2>Update personal information</h2>
 
-  <div class = "input-container">
-    <i class = "fa fa-user-circle icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "fname" value = "'.$fname.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user-circle icon"></i>
+    <input class="input-field" type="text" name="fname" value="<?php echo $fname; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-user-circle icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "lname" value = "'.$lname.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user-circle icon"></i>
+    <input class="input-field" type="text" name="lname" value="<?php echo $lname; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-envelope icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "email" name = "email" value = "'.$email.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-envelope icon"></i>
+    <input class="input-field" type="email" name="email" value="<?php echo $email; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-user icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "username" value = "'.$username.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user icon"></i>
+    <input class="input-field" type="text" name="username" value="<?php echo $username; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-mobile icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" maxlength = "15" placeholder = "Enter mobile number" name = "phone" required value = "'.$phone.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-mobile icon"></i>
+    <input class="input-field" type="text" maxlength="15" placeholder="Enter mobile number" name="phone" value="<?php echo $phone; ?>"
+    required readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-file-text icon"></i>
-    <select class = "input-field" id  =  "purpose" name = "purpose" required>
-      <option value = "" disabled selected>Purpose of joining</option>
-      <option value = "Weight gain">Weight Gain</option>
-      <option value = "Weight loss">Weight loss</option>
-      <option value = "Toning">Toning</option>
-      <option value = "Stay fit">Stay fit</option>
+  <div class="input-container">
+    <i class="fa fa-file-text icon"></i>
+    <select class="input-field" id="purpose" name="purpose" required>
+      <option value="" disabled>Purpose of joining</option>
+      <?php
+        $PURPOSE = [
+          "Weight gain",
+          "Weight loss",
+          "Toning",
+          "Stay fit",
+        ];
+        foreach ($PURPOSE as $i => $t) {
+          $selected = ($t == $purpose) ? " selected" : "";
+          echo '<option value="' . $t . '"' . $selected . '>' . $t . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-inr icon"></i>
-    <select class = "input-field" id = "fee"  name = "fee" required>
-      <option value = "" disabled selected>Choose plan</option>
-      <option value = "2000">Monthly Plan - Rs. 2000</option>
-      <option value = "1800">Quaterly Plan - Rs. 5400 for 3 months</option>
-      <option value = "1700">Special Package - Rs. 9600 for 6 months</option>
-      <option value = "1500">Annual Package - Rs. 18000</option>
+  <div class="input-container">
+    <i class="fa fa-inr icon"></i>
+    <select class="input-field" id="fee" name="fee" required>
+      <option value="" disabled>Choose plany</option>
+      <?php
+        $FEE = [
+          "2000" => "Monthly Plan - Rs. 2000",
+          "1800" => "Quarterly Plan - Rs. 5400 for 3 months",
+          "1700" => "Special Package - Rs. 9600 for 6 months",
+          "1500" => "Annual Package - Rs. 18000"
+        ];
+        foreach ($FEE as $key => $val) {
+          $selected = ($key == $fee) ? " selected" : "";
+          echo '<option value="' . $key . '"' . $selected . '>' . $val . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-balance-scale icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "number" placeholder = "Enter weight" min = "30" max = "999" name = "weight"required value = "'.(int)$weight.'">
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-balance-scale icon"></i>
+    <input class="input-field" type="number" placeholder="Enter weight" min="30" max="999" name="weight" value="<?php echo (int)$weight; ?>"
+      required>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-child icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "number" placeholder = "Enter Height in meters (upto 3 decimal places)" min = "0" max = "5" step = "0.001" name = "height" required value = "'.(float)$height.'">
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-child icon"></i>
+    <input class="input-field" type="number" placeholder="Enter Height in meters (upto 3 decimal places)" min="0" max="5" step="0.001"
+      name="height" value="<?php echo (float)$height; ?>" required>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-user-md icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" maxlength = "100" placeholder = "Any notable medical history" name = "history" name = "history" required value = "'.$history.'">
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user-md icon"></i>
+    <input class="input-field" type="text" maxlength="100" placeholder="Any notable medical history" name="history" name="history"
+      required value="<?php echo $history; ?>">
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-file-text icon"></i>
-    <?php echo '
-    <input class = "input-field" type = "text" name = "joined" value = "'.$joined.'" readonly>';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-file-text icon"></i>
+    <input class="input-field" type="text" name="joined" value="<?php echo $joined; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-male icon"></i>
-    <?php echo '
-    <input class = "input-field" type = "text" placeholder = "No trainer assigned yet" name = "trainer" value = "'.$trainer.'" readonly>';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-male icon"></i>
+    <input class="input-field" type="text" placeholder="No trainer assigned yet" name="trainer" value="<?php echo $trainer; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-male icon"></i>
-    <?php echo '
-    <input class = "input-field" type = "text" placeholder = "No trainer assigned yet" name = "trainer" value = "'.$trainer.'" readonly>';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-male icon"></i>
+    <input class="input-field" type="text" placeholder="No trainer assigned yet" name="trainer" value="<?php echo $trainer; ?>" readonly disabled>
   </div>
 
- <div class = "boxx-container">
-  <h3><br><br><br>Diet chart</h3><hr>
-  <div class = "boxx">
-    <?php
-      echo '<p>'.$chart.'</p>';
-    ?>
+  <div class = "boxx-container">
+    <h3><br><br><br>Diet chart</h3>
+    <hr>
+    <div class = "boxx">
+      <p><?php echo $chart; ?></p>
+    </div>
   </div>
-</div>
-<br>
 
-<button type = "submit" name = "update" class = "btn">Update Information</button>
+  <br>
+
+  <button type = "submit" name = "update" class = "btn">Update Information</button>
 </form>
 <br>
 
-<form action = "./change_password.php" method = "POST" style = "max-width:500px;margin:auto">
-  <button type = "submit" name = "changePassword" class = "btn">Change password</button>
-</form>
+<a href="change_password.php" style="max-width:500px; margin: 0 auto; display:block" class="btn">Change password</a>
 
 <?php
 
--- trainer/edit_details.php
+++ trainer/edit_details.php
@@ -1,5 +1,4 @@
 <?php
-
   session_start();
   if($_SESSION['type'] != "trainer") {
     header("Location: ../index.php");
@@ -10,27 +9,20 @@
   require('../includes/db.php');
 
   $status = $_GET['edit'];
-
-  if($status == "success") {
 ?>
 
-    <div class="alert alert-success">
-      <strong>Success!</strong> Information updated successfully
-    </div>
-<?php
-
-    }
-  elseif($status == "error") {
-?>
-
-<div class="alert alert-danger">
-  <strong>Error!</strong> Could not update details
-</div>
+<?php if($status == "success") { ?>
+  <div class="alert alert-success">
+    <strong>Success!</strong> Information updated successfully
+  </div>
+<?php } elseif($status == "error") { ?>
+  <div class="alert alert-danger">
+    <strong>Error!</strong> Could not update details
+  </div>
+<?php } ?>
 
 <?php
-  }
-
-  $getDetails = "SELECT fname, lname, m.username, email, phone, joined, password, experience
+  $getDetails = "SELECT fname, lname, m.username, email, phone, joined, `password`, experience, `type`, salary, specialization, qualification
   from trainer t, members m
   WHERE m.username=? AND t.username = m.username";
 
@@ -39,130 +31,136 @@
   mysqli_stmt_bind_param($stmt1, "s", $_SESSION['uid']);
   mysqli_stmt_execute($stmt1);
 
-  mysqli_stmt_bind_result($stmt1, $fname, $lname, $username, $email, $phone, $joined,
-  $password, $experience);
+  mysqli_stmt_bind_result($stmt1, $fname, $lname, $username, $email, $phone, $joined, $password, $experience, $type, $salary, $specialization, $qualification);
 
   mysqli_stmt_fetch($stmt1);
   mysqli_stmt_close($stmt1);
 ?>
 
-<link rel = "stylesheet" href = "/gym_management/assets/css/font-awesome.min.css"></link>
+<link rel="stylesheet" href="/gym_management/assets/css/font-awesome.min.css"></link>
 <link rel="stylesheet" type="text/css" href="/gym_management/assets/css/trainer.css"></link>
 
 
-<form action = "./includes/edit_detailsI.php" method = "POST" style = "max-width:500px;margin:auto">
+<form action="./includes/edit_detailsI.php" method="POST" style="max-width:500px;margin:auto">
   <h2>Update personal information</h2>
 
-  <div class = "input-container">
-    <i class = "fa fa-user-circle icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "fname" value = "'.$fname.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user-circle icon"></i>
+    <input class="input-field" type="text" name="fname" value="<?php echo $fname; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-user-circle icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "lname" value = "'.$lname.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user-circle icon"></i>
+    <input class="input-field" type="text" name="lname" value="<?php echo $lname; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-envelope icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "email" name = "email" value = "'.$email.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-envelope icon"></i>
+    <input class="input-field" type="email" name="email" value="<?php echo $email; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-user icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" name = "username" value = "'.$username.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-user icon"></i>
+    <input class="input-field" type="text" name="username" value="<?php echo $username; ?>" readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-mobile icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "text" maxlength = "15" placeholder = "Enter mobile number" name = "phone" required value = "'.$phone.'" readonly>
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-mobile icon"></i>
+    <input class="input-field" type="text" maxlength="15" placeholder="Enter mobile number" name="phone" value="<?php echo $phone; ?>"
+    required readonly disabled>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-file-text icon"></i>
-    <select class = "input-field" id  =  "type" name = "type" required>
-      <option value = "" disabled selected>Trainer type</option>
-      <option value = "Personal Trainer(Hourly)">Personal Trainer(Contingent-Hourly)</option>
-      <option value = "Personal Trainer">Personal Trainer</option>
-      <option value = "Group Trainer">Group Trainer</option>
+  <div class="input-container">
+    <i class="fa fa-file-text icon"></i>
+    <select class="input-field" id="type" name="type" required>
+      <option value="" disabled>Trainer type</option>
+      <?php
+        $TYPE = [
+          "Personal Trainer(Hourly)",
+          "Personal Trainer",
+          "Group Trainer"
+        ];
+        foreach ($TYPE as $i => $t) {
+          $selected = ($t == $type) ? " selected" : "";
+          echo '<option value="' . $t . '"' . $selected . '>' . $t . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-inr icon"></i>
-    <select class = "input-field" id = "salary"  name = "salary" required>
-      <option value = "" disabled selected>Choose Expected Salary</option>
-      <option value = "2000">Below Rs. 2000</option>
-      <option value = "3000">Rs. 2000 - Rs. 3000</option>
-      <option value = "4000">Rs. 3000 - Rs. 4000</option>
-      <option value = "5000">Above Rs. 4000</option>
+  <div class="input-container">
+    <i class="fa fa-inr icon"></i>
+    <select class="input-field" id="salary" name="salary" required>
+      <option value="" disabled>Choose Expected Salary</option>
+      <?php
+        $SALARIES = [
+          "2000" => "Below Rs. 2000",
+          "3000" => "Rs. 2000 - Rs. 3000",
+          "4000" => "Rs. 3000 - Rs. 4000",
+          "5000" => "Above Rs. 4000"
+        ];
+        foreach ($SALARIES as $key => $val) {
+          $selected = ($key == $salary) ? " selected" : "";
+          echo '<option value="' . $key . '"' . $selected . '>' . $val . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-certificate icon"></i>
-    <select class = "input-field" id = "specialization"  name = "specialization" required>
-      <option value = "" disabled selected>Choose Specialization</option>
-      <option value = "Nutrition and Weight Management">Nutrition and Weight Management</option>
-      <option value = "Clinical Disease">Clinical Disease</option>
-      <option value = "Mind-Body Fitness">Mind-Body Fitness</option>
-      <option value = "Special Populations">Special Populations</option>
+  <div class="input-container">
+    <i class="fa fa-certificate icon"></i>
+    <select class="input-field" id="specialization" name="specialization" required>
+      <option value="" disabled selected>Choose Specialization</option>
+      <?php
+        $SPECIALIZATION = [
+          "Nutrition and Weight Management",
+          "Clinical Disease",
+          "Mind-Body Fitness",
+          "Special Populations"
+        ];
+        foreach ($SPECIALIZATION as $i => $t) {
+          $selected = ($t == $specialization) ? " selected" : "";
+          echo '<option value="' . $t . '"' . $selected . '>' . $t . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-book icon"></i>
-    <select class = "input-field" id = "qualification"  name = "qualification" required>
-      <option value = "" disabled selected>Choose Qualification</option>
-      <option value = "12th pass">12th Pass</option>
-      <option value = "Diploma">Diploma</option>
-      <option value = "Master's Degree">Master's Degree</option>
-      <option value = "Professioal Degree">Professioal Degree</option>
+  <div class="input-container">
+    <i class="fa fa-book icon"></i>
+    <select class="input-field" id="qualification" name="qualification" required>
+      <option value="" disabled selected>Choose Qualification</option>
+      <?php
+        $QUALIFICATION = [
+          "12th pass",
+          "Diploma",
+          "Master's Degree",
+          "Professioal Degree"
+        ];
+        foreach ($QUALIFICATION as $i => $t) {
+          $selected = ($t == $qualification) ? " selected" : "";
+          echo '<option value="' . $t . '"' . $selected . '>' . $t . '</option>';
+        }
+      ?>
     </select>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-black-tie icon"></i>
-    <?php
-        echo '
-          <input class = "input-field" type = "number" placeholder = "Enter experience" min = "0" max = "30" name = "experience"required value = "'.(int)$experience.'">
-        ';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-black-tie icon"></i>
+    <input class="input-field" type="number" placeholder="Enter experience" min="0" max="30" name="experience" value="<?php echo (int)$experience ?>"
+      required>
   </div>
 
-  <div class = "input-container">
-    <i class = "fa fa-file-text icon"></i>
-    <?php echo '
-    <input class = "input-field" type = "text" name = "joined" value = "'.$joined.'" readonly>';
-    ?>
+  <div class="input-container">
+    <i class="fa fa-file-text icon"></i>
+    <input class="input-field" type="text" name="joined" value="<?php echo $joined ?>" readonly disabled>
   </div>
+
+  <br>
+
+  <button type="submit" name="update" class="btn">Update Information</button>
+</form>
 
 <br>
 
-<button type = "submit" name = "update" class = "btn">Update Information</button>
-</form>
-<br>
-
-<form action = "./change_password.php" method = "POST" style = "max-width:500px;margin:auto">
-  <button type = "submit" name = "changePassword" class = "btn">Change password</button>
-</form>
+<a href="change_password.php" style="max-width:500px; margin: 0 auto; display:block" class="btn">Change password</a>
